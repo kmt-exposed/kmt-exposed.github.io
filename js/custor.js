18fc00facc53d1a5363a52a7805a8a2c00136b0b
billy3321 <billy3321@gmail.com>
2017-05-11 01:41:37 +0800

    add jeykll files

--- js/custor.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-$(document).ready(function() {
-    $(".toggle").click(function() {
-        $(this).toggleClass("active");
-        $("#navbar-nav").slideToggle();
-    });
-});
